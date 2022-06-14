ac3aa894c2ce98d96ae4291cad2e93fbb85b3f99
mikzovi <mikzovi@ya.ru>
2022-06-14 18:57:58 +0300

    Import CSV file added

--- importDB.py
+++ importDB.py
@@ -1,20 +1,37 @@
 
 
-def import_txt(data):
-    return
-
-def import_csv(data):
-    return
-
-def import_json(data):
-    return
+def import_txt():
+    path_to_file = 'phonebook.txt'
+    return print(path_to_file)
 
 
-def import_data(data):
-    # тут будет главное тело
-    # определяем какой формат данных и выбираем для него функцию импорта
-    
+def import_csv():
+    import csv
+    path_to_file = 'phonebook.csv'
+    results = []
+    with open (path_to_file) as csvfile:                           
+        # Создаем объект reader, указываем символ-разделитель ";"
+        file_reader = csv.reader(csvfile, delimiter= ";")
+        for row in file_reader:
+            results.append(row) # Каждую строку из телефонной книги добавляем в список результатов
+    return results
 
-    return
+def import_json():
+    path_to_file = 'phonebook.json'
+    return print(path_to_file)
 
-# Test
+# Выбираем формат файла телефонной книги
+def choice_format():
+    print('Телефонную книгу в каком формате будете импотировать? \n.txt - 1 .csv - 2 .json - 3:')
+    choice = int((input()))
+    match choice:
+        case 1: return import_txt()
+        case 2: return import_csv()
+        case 3: return import_json()
+        case _ :
+            print('Неизвестный формат файла')
+            exit()
+
+choice_format()
+print(import_csv())   
+
